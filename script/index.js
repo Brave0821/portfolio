2a05ba44d2c994efa2ad74245e2c955fbf43c250
Brave <august8821@naver.com>
2024-02-29 18:13:26 +0900

    중간저장!!

--- script/index.js
+++ script/index.js
@@ -3,7 +3,7 @@
 /* 상단 메인 슬라이드 */
 const my_swiper = new Swiper('#projects', { 
     /* autoplay:{delay:3000}, */
-    slidesPerView: 5, //슬라이드 한번에 1개씩 보이기
+    slidesPerView: 3, //슬라이드 한번에 1개씩 보이기
     spaceBetween:0, // 슬라이드 여백
     centeredSlides:true, // 슬라이드의 기준을 가운데부터 시작하도록 설정하기
     loop:true,
@@ -17,16 +17,35 @@ const my_swiper = new Swiper('#projects', {
 })
 
 /* 스크롤 버튼 */
-document.addEventListener("DOMContentLoaded", function() {
-    const scroll_btn = document.querySelector(".scroll_btn")
-    const start_wrap = this.document.querySelector(".start_wrap")
 
-    scroll_btn.addEventListener("click",function() {
-        start_wrap.scrollIntoView({behavior: 'smooth'});
-    })
+/* 메일문의  */
+const mail_inquire = document.querySelector(".mail a")
+const mail_contents = document.querySelector(".mail form")
+let is_visible = false;
+console.log(mail_inquire, mail_contents)
 
-    window.scrollTo({
-        top: document.body.scrollHeight,
-        behavior: 'smooth' // 부드럽게 스크롤되도록 설정
-    });
-});
+
+mail_inquire.addEventListener("click",()=>{
+    event.preventDefault(); // 기본 동작 방지
+    if (is_visible) {
+        mail_contents.style.display = "none"// 클릭 시 숨기기
+        mail_inquire.querySelector("img").src =  "../images/icon/chevron-up-solid.svg"
+    } else {
+        mail_inquire.querySelector("img").src =  "../images/icon/chevron-down-solid.svg"
+        mail_contents.style.display = "block" // 숨겨진거 보이기
+    }
+    is_visible = !is_visible; //상태변경
+}); 
+
+/* 주소 */
+/* 기획 페이지 팝업으로 나오게 만들기  */
+
+
+
+/* const button 변수 만들기 */ 
+
+/* button.addEventListener("click", ()=>{
+    window.open("coin_charging_page.html", "popup", "width=699,height=1377")
+}) */
+
+/* 결과는 a에 링크줘서  */
