11f2f5ff8f8d60b5e85e4106e239310b3b8e5dfe
Brave <august8821@naver.com>
2024-06-04 15:51:30 +0900

    스타벅스 진행도 수정 90%

--- script/index.js
+++ script/index.js
@@ -160,7 +160,7 @@ function updateProgress_star(percentage) {
         progressBar_star.style.backgroundColor = '#4caf50'; // Green color for contribution above or equal to 60%
     }
   }
-  updateProgress_star(80);
+  updateProgress_star(90);
 
 
 
